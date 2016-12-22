66efa93a8d5c1123db93a530d229aa910a6d31bb
elvis <elvis@lnmpy.com>
2016-12-22 15:37:41 +0800

    remove team_id and empty token for public share slack command

--- flask_slack/slack.py
+++ flask_slack/slack.py
@@ -7,44 +7,35 @@ class Slack(object):
 
     def __init__(self, app=None):
         self._commands = {}
-        self.team_id = None
 
         if app:
             self.init_app(app)
 
     def init_app(self, app=None):
         """Initialize application configuration"""
+        self._app = app
         config = getattr(app, 'config', app)
+        self._config = config
 
-        self.team_id = config.get('TEAM_ID')
-
-    def command(self, command, token, team_id=None, methods=['GET'], **kwargs):
+    def command(self, command, token=None, methods=['GET'], **kwargs):
         """A decorator used to register a command.
         Example::
 
             @slack.command('your_command', token='your_token',
-                           team_id='your_team_id', methods=['POST'])
+                           methods=['POST'])
             def your_method(**kwargs):
                 text = kwargs.get('text')
                 return slack.response(text)
 
         :param command: the command to register
         :param token: your command token provided by slack
-        :param team_id: optional. your team_id provided by slack.
-                        You can also specify the "TEAM_ID" in app
-                        configuration file for one-team project
         :param methods: optional. HTTP methods which are accepted to
                         execute the command
         :param kwargs: optional. the optional arguments which will be passed
                        to your register method
         """
-        if team_id is None:
-            team_id = self.team_id
-        if team_id is None:
-            raise RuntimeError('TEAM_ID is not found in your configuration!')
-
         def deco(func):
-            self._commands[(team_id, command)] = (func, token, methods, kwargs)
+            self._commands[command] = (func, token, methods, kwargs)
             return func
         return deco
 
@@ -64,42 +55,40 @@ class Slack(object):
             data = request.form
 
         token = data.get('token')
-        team_id = data.get('team_id')
         command = data.get('command') or data.get('trigger_word')
 
         if isinstance(command, string_types):
             command = command.strip().lstrip('/')
 
         try:
-            self.validate(command, token, team_id, method)
+            self.validate(command, token, method)
         except SlackError as e:
             return self.response(e.msg)
 
-        func, _, _, kwargs = self._commands[(team_id, command)]
+        func, _, _, kwargs = self._commands[command]
         kwargs.update(data.to_dict())
 
         return func(**kwargs)
 
     dispatch.methods = ['GET', 'POST']
 
-    def validate(self, command, token, team_id, method):
+    def validate(self, command, token, method):
         """Validate request queries with registerd commands
 
         :param command: command parameter from request
         :param token: token parameter from request
-        :param team_id: team_id parameter from request
         :param method: the request method
         """
-        if (team_id, command) not in self._commands:
-            raise SlackError('Command {0} is not found in team {1}'.format(
-                             command, team_id))
+        if command not in self._commands:
+            raise SlackError('Command {0} is not found'.format(
+                             command))
 
-        func, _token, methods, kwargs = self._commands[(team_id, command)]
+        func, _token, methods, kwargs = self._commands[command]
 
         if method not in methods:
             raise SlackError('{} request is not allowed'.format(method))
 
-        if token != _token:
+        if _token and token != _token:
             raise SlackError('Your token {} is invalid'.format(token))
 
     def response(self, text, response_type='ephemeral', attachments=None):
